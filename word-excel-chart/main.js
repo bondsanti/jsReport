af98d6bc7226d2fe40a08af301b0be07d02c8e92
santidev <santi.chpy@gmail.com>
2023-01-26 00:11:04 +0700

    update

--- word-excel-chart/main.js
+++ word-excel-chart/main.js
@@ -1,16 +1,48 @@
 import './style.css'
+import ApexCharts from 'apexcharts'
 import javascriptLogo from './javascript.svg'
 import { genDocx1 } from './docx1'
 import { genDocx2 } from './docx2'
 import { genExcel } from './excel'
 import { genCsv } from './csv'
 
+window.addEventListener("load",(event)=>{
+  let options = {
+    series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  xaxis: {
+    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+      'United States', 'China', 'Germany'
+    ],
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+})
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    
+    <div id="chart">
+    </div>
+    <hr>
     <div class="card">
       <button id="btn1" type="button">Docx 1</button>
       <button id="btn2" type="button">Docx 2</button>
