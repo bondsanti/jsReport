46ff5299c886a5ae9efea0a005bfeb810e5743ca
santidev <santi.chpy@gmail.com>
2023-01-25 18:34:26 +0700

    genword

--- word-excel-chart/docx1.js
+++ word-excel-chart/docx1.js
@@ -1,8 +1,38 @@
-
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import { saveAs } from "file-saver";
 
 export function genDocx1(element) {
 
+  //const d = new Date()
   element.addEventListener('click', () =>{
-    alert("hello world")
+    const doc = new Document({
+      sections: [
+          {
+              properties: {},
+              children: [
+                  new Paragraph({
+                      children: [
+                          new TextRun("Hello World"),
+                          new TextRun({
+                              text: "Foo Bar",
+                              bold: true,
+                          }),
+                          new TextRun({
+                              text: "\tGithub is the best",
+                              bold: true,
+                          }),
+                      ],
+                  }),
+              ],
+          },
+      ],
   });
+  Packer.toBlob(doc).then((blob) => {
+    saveAs(blob,"example.docx");
+  });
+  
+  });
+
+
+
 }
